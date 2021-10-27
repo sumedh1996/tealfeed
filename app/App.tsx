b84da0eed58d44cc5d8bda08dc6c676ede845583
sumedh raj <sumedh@tealfeed.com>
2021-10-28 05:28:14 +0530

    Added Tailwind Config

--- app/App.tsx
+++ app/App.tsx
@@ -1,15 +1,18 @@
 import {NavigationContainer} from '@react-navigation/native';
+import {Provider as PaperProvider} from 'react-native-paper';
 import React, {Component} from 'react';
 import {Text, View} from 'react-native';
 
 export class App extends Component {
   render() {
     return (
-      <NavigationContainer>
-        <View>
-          <Text> This is the Initial Routing Commit </Text>
-        </View>
-      </NavigationContainer>
+      <PaperProvider>
+        <NavigationContainer>
+          <View>
+            <Text> This is the Initial Routing Commit </Text>
+          </View>
+        </NavigationContainer>
+      </PaperProvider>
     );
   }
 }
