35ee689777926b9c8c0a1814e2b1a969428a6e53
sumedh raj <sumedh@tealfeed.com>
2021-11-08 21:39:39 +0530

    Fixed App

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,79 @@
-import * as React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
+import {
+  NavigationContainer,
+  DarkTheme as NavigationDarkTheme,
+  DefaultTheme as NavigationDefaultTheme,
+} from '@react-navigation/native';
+import {
+  DarkTheme as PaperDarkTheme,
+  DefaultTheme as PaperDefaultTheme,
+  Card,
+  Title,
+  Paragraph,
+  List,
+  Provider as PaperProvider,
+} from 'react-native-paper';
+import merge from 'deepmerge';
+import {createStackNavigator} from '@react-navigation/stack';
+import {TouchableOpacity} from 'react-native';
+
+import React from 'react';
+import {PreferencesContext} from './contexts/ThemeContext';
+
+const HomeScreen = ({navigation}: any) => (
+  <TouchableOpacity
+    onPress={() =>
+      navigation?.push('Details', {title: 'Test', content: 'jhdfvjhadjh'})
+    }>
+    <Card>
+      <Card.Content>
+        <Title>Test</Title>
+        <Paragraph>Multiple Test</Paragraph>
+      </Card.Content>
+    </Card>
+  </TouchableOpacity>
+);
+
+const DetailsScreen = ({props}: any) => {
+  const {title, content} = props?.route?.params;
+  return (
+    <List.Section>
+      <List.Subheader>{title}</List.Subheader>
+      <List.Item title={content} />
+    </List.Section>
+  );
+};
+
+const CombinedDefaultTheme = merge(PaperDefaultTheme, NavigationDefaultTheme);
+const CombinedDarkTheme = merge(PaperDarkTheme, NavigationDarkTheme);
+
+const Stack = createStackNavigator();
 
 export default function App() {
+  const [isThemeDark, setIsThemeDark] = React.useState(false);
+
+  let theme = isThemeDark ? CombinedDarkTheme : CombinedDefaultTheme;
+
+  const toggleTheme = React.useCallback(() => {
+    return setIsThemeDark(!isThemeDark);
+  }, [isThemeDark]);
+
+  const preferences = React.useMemo(
+    () => ({
+      toggleTheme,
+      isThemeDark,
+    }),
+    [toggleTheme, isThemeDark],
+  );
   return (
-    <NavigationContainer>{/* Rest of your app code */}</NavigationContainer>
+    <PreferencesContext.Provider value={preferences}>
+      <PaperProvider theme={theme}>
+        <NavigationContainer theme={theme}>
+          <Stack.Navigator initialRouteName="Home">
+            <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="Details" component={DetailsScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </PaperProvider>
+    </PreferencesContext.Provider>
   );
 }
